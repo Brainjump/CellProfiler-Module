db4ac99d9d13da66af82d53405835bda588b786b
Foulon Nicolas <nnfoulon@gmail.com>
2013-09-24 15:13:59 +0200

    MyModule display the 4 images of the ZVI file. file must be open with option "individual image" in loadImages.

--- MyModule.py
+++ MyModule.py
@@ -19,22 +19,49 @@ class MyModule(cpm.CPModule):
     # module_name and category attributes must be added on your own classes
     #===========================================================================
     module_name = "MyModule"
+    variable_revision_number = 1 #Version of the module (use for save Pipeline)
     category = "Plugin Module"
     def run(self, workspace):
         print "Hello i'm a new module and i'm running"
-        #=======================================================================
-        # image = workspace.image_set.get_image(self.image_name.value,
-        #                                       must_be_grayscale = True)
-        #=======================================================================
+        image = workspace.image_set.get_image(self.image_name.value)
+        image_collection = []
+        print "dimension of array = %d" % image.pixel_data.ndim
+        image_number = image.pixel_data.shape
+        print "The zvi got %d images" % image_number[2]
+        for index in range(image_number[2]):
+            title = ""
+            if index == 0:
+                title = "Original"
+            elif index == 1:
+                title = "Red"
+            elif index == 2:
+                title = "Green"
+            elif index == 3:
+                title = "Blue"
+                 
+            image_collection.append((image.pixel_data[:,:,index], title))
+        
+        workspace.display_data.image_collection = image_collection
+
+
+    def display(self, workspace):
+        """display the result on a new frame. execute after run()"""
+        
+        image_collection = workspace.display_data.image_collection
+        
+        
+        
         window_name = "CellProfiler:%s:%d"%(self.module_name,self.module_num)
         figure = workspace.create_or_find_figure(title="MyModule Display",
                                                  window_name = window_name,
-                                                 subplots = (1,1))
-        image = workspace.image_set.get_image(self.image_name.value)
-        title = "Dysplay"
-        figure.subplot_imshow_grayscale(0,0, image.pixel_data, title)
+                                                 subplots = (2,2))
         
+        layout = [(0,0), (1,0), (0,1), (1,1)]
         
+        for xy, image in zip(layout, image_collection):
+            figure.subplot_imshow_grayscale(xy[0], xy[1], image[0], image[1])
+        
+    
     def create_settings(self):
         """This method create the settings. This use to set the diverse buttons"""
         
@@ -50,6 +77,8 @@ class MyModule(cpm.CPModule):
  
         return self.__settings
 
+
+        
     #===========================================================================
     # def display(self, workspace):
     #     figure = workspace.create_or_find_figure(title="MyModule Display", )
