0e8ebb2e4a1c82be13756013548bf0202d8df999
BrainJump <nnfoulon@gmail.com>
2013-11-07 11:50:54 +0100

    commit for save

--- MyModule.py
+++ MyModule.py
@@ -21,8 +21,7 @@ import scipy.ndimage.morphology as scipym
 import numpy as np
 import scipy
 from scipy import ndimage
-
-import skimage.feature as skf
+import matplotlib.pyplot as plt
 
 
 class MyModule(cpm.CPModule):
@@ -34,17 +33,26 @@ class MyModule(cpm.CPModule):
     module_name = "MyModule"
     variable_revision_number = 1 #Version of the module (use for save Pipeline)
     category = "Plugin Module"
+
     
     def run(self, workspace):
-    
+        
+        
         image = workspace.image_set.get_image(self.image_name.value)
         image_collection = []
         print "dimension of array = %d" % image.pixel_data.ndim
+        #
+        #Test if the image was open with option "individual image" in module LoadImages
+        #
         if  not image.pixel_data.ndim == 3:
             from moduleException import ModuleException
             raise ModuleException(1, "Image must be load with the module LoadImage and the option \"individual image\"")
         image_number = image.pixel_data.shape
         print "The zvi got %d images" % image_number[2]
+        
+        #
+        #Creation of the image_collection
+        #
         for index in range(image_number[2]):
             title = ""
             if index == 0:
@@ -59,63 +67,97 @@ class MyModule(cpm.CPModule):
             image_collection.append((image.pixel_data[:,:,index], title))
         
         
-        image_collection[1] = (image_collection[3][0], "original")
-        image_collection[0] = (image_collection[2][0], "original")
+        image_collection[1] = (image_collection[3][0], "original Elf3B")
+        image_collection[0] = (image_collection[2][0], "original DAPI")
+ 
         #
-        #Get the global Threshold with Otsu algorithm
+        #Get the global Threshold with Otsu algorithm and smooth nuclei image
         #
-       
-        global_threshold = otsu(image_collection[3][0], min_threshold=0, max_threshold=1)
-        print "the threshold compute by the Otsu algorythm is %f" % global_threshold
+#         nuclei_smoothed = self.smooth_image(image_collection[3][0], image.mask, 1)
+        nuclei_smoothed = image_collection[3][0] 
+        global_threshold_nuclei = otsu(nuclei_smoothed, min_threshold=0, max_threshold=1)
+        print "the threshold compute by the Otsu algorythm is %f" % global_threshold_nuclei      
         
         
-        image_smoothed = self.smooth_image(image_collection[3][0], image.mask, 1)
-        
         #
-        #Binary the "Blue" Image 
+        #Binary the "DAPI" Image (Nuclei) and labelelize the nuclei
         #
-        binary_image = np.logical_and((image_smoothed >= global_threshold), image.mask)
-        
-        #
-        #label the previous image.
-        #
-        labeled_image, object_count = scipy.ndimage.label(binary_image, np.ones((3,3), bool))
+
+        binary_nuclei = (nuclei_smoothed >= global_threshold_nuclei)
+        labeled_nuclei, object_count = scipy.ndimage.label(binary_nuclei, np.ones((3,3), bool))
         print "the image got %d detected" % object_count
         
+        #
+        #Fill the hole and delete object witch touch the border. 
+        #labeled_nuclei is modify after the function
+        #Filter small object and split object
+        #
+        labeled_nuclei = fill_labeled_holes(labeled_nuclei)        
+        labeled_nuclei = self.filter_on_border(labeled_nuclei)
+        labeled_nuclei = self.filter_on_size(labeled_nuclei, object_count)         
+        labeled_nuclei = self.split_object(labeled_nuclei)
         
         #
-        #Fill the hole
+        #Edge detection of nuclei image and object are more separated 
         #
-        labeled_image = fill_labeled_holes(labeled_image)        
+        labeled_nuclei_canny = skf.sobel(labeled_nuclei)       
+        labeled_nuclei[labeled_nuclei_canny > 0] = 0
+        labeled_nuclei = skr.minimum(labeled_nuclei.astype(np.uint16), skm.disk(3))
         
-        #
-        #delete object witch touch the border. labeled_image is modify after the function
-        #
-        labeled_image = self.filter_on_border(labeled_image)
+        # 
+        #segmentation of the Cell in the Elf3B image
+        #      
+        cell_image = image_collection[0][0]
+        
+        cell_threshold = otsu(cell_image,
+                              min_threshold=0,
+                              max_threshold=1)
+        cell_bg_mark = skm.binary_erosion((cell_image < cell_threshold).astype(np.uint8),                                   
+                                          skm.disk(5))
         
 
-        #Filter small object.
-        labeled_image = self.filter_on_size(labeled_image, object_count)
+        marker = labeled_nuclei.copy()
+        number_labeled_nuclei = np.max(marker)
+        marker[cell_bg_mark] = number_labeled_nuclei + 1
         
-        image_collection[2] = (labeled_image.copy(), image_collection[2][1])
-        labeled_image = self.split_object2(labeled_image)
-        #
+        cell_gradient = skr.gradient(cell_image.astype(np.uint8), skm.disk(3))
+        
+        print "cell threshold = ", cell_threshold
+
+
+#         cell_binary = skr.maximum(cell_binary.astype(np.uint16), skm.disk(5))
+#         cell_binary = fill_labeled_holes(cell_binary)
+#         cell_binary = skm.binary_erosion(cell_binary, skm.disk(5))
+#         distance = scipym.distance_transform_edt(cell_binary).astype(np.uint16)
+#         labeled_nuclei[labeled_nuclei == labeled_nuclei[0][0]] = 0
+        
         #Set the image_collection attribute for display
-        #
-        new_blue = (labeled_image, image_collection[3][1])
         
-        image_collection[3] = new_blue
+        
+        labeled_cell = skm.watershed(cell_gradient, marker)
+         
+        new_labeled_elf3b = (labeled_cell, image_collection[2][1])
+
+        new_labeled_dapi = (labeled_nuclei, image_collection[3][1])
+        
+        canny_for_test = image_collection[1][0].copy()
+        canny_for_test = skf.sobel(canny_for_test)
+#         canny_for_test_otsu = otsu(canny_for_test, min_threshold=0, max_threshold=1)
+#         canny_for_test = (canny_for_test >= canny_for_test_otsu)
+        
+        plt.figure()
+        plt.imshow(canny_for_test)
+        plt.show()
+        
+        image_collection[2] = new_labeled_elf3b
+        image_collection[3] = new_labeled_dapi
         workspace.display_data.image_collection = image_collection
-
-
 
 
     def display(self, workspace, figure=None):
         """display the result on a new frame. execute after run()"""
         
         image_collection = workspace.display_data.image_collection
-        
-        
         if len(image_collection) == 4:
             layout = [(0,0), (1,0), (0,1), (1,1)]
         else:
@@ -136,12 +178,14 @@ class MyModule(cpm.CPModule):
                 figure.subplot_imshow_labels(xy[0], xy[1], image[0], image[1])
             else:
                 figure.subplot_imshow_grayscale(xy[0], xy[1], image[0], image[1])
-        
+        #=======================================================================
+        # for thread in self.thread_list:
+        #     thread.wait()
+        #=======================================================================
     
     def create_settings(self):
         """This method create the settings. This use to set the diverse buttons"""
         
-        #print "Hey, i create the sereturn split_imagettings for this new module"
         self.image_name = cps.ImageNameSubscriber(
             "Select the input image",doc = """
             Choose the image to be cropped.""")
@@ -168,7 +212,7 @@ class MyModule(cpm.CPModule):
         #Get the histogram
         #First: create a coo_matrix (it's just a format: 3 columns. 1 for the row, 1 for the column, the last for the value)
         #here row[i] = border_label[i], column[i] = 0, value[i] = 1
-        # todense() create the matrix. create a Matrix initialized at 0, with the shape given.
+        # todense() create the matrixtwitter rebublic of gamer. create a Matrix initialized at 0, with the shape given.
         #then add the value[i] at the position: (row[i], column[i])
         #the trick here is column value is always 0, and the value always 1.
         #So that create the histogram. flatten just convert the matrix into array.
@@ -178,11 +222,11 @@ class MyModule(cpm.CPModule):
         histogram = scipy.sparse.coo_matrix((np.ones(border_labels.shape),
                                              (border_labels,
                                               np.zeros(border_labels.shape))),
-                                             shape=(np.max(labeled_image)+1,1)).todense()
+                                              shape=(np.max(labeled_image)+1,1)).todense()
                                              
         histogram = np.array(histogram).flatten()
         
-        if any(histogram[1:] > 0 ):
+        if np.any(histogram[1:] > 0 ):
             histogram_image = histogram[labeled_image]
             labeled_image[histogram_image > 0] = 0
         return labeled_image
@@ -200,33 +244,14 @@ class MyModule(cpm.CPModule):
         labeled_image[area_image < min_allowed_area] = 0
         
         return labeled_image
-
-    def split_object1(self, labeled_image):
-        """ split object when it's necessary
-        """
         
-        labeled_image = labeled_image.astype(np.uint16)
-        #binary the Labeled_image
-        binary = np.logical_and((labeled_image > 0), np.ones(labeled_image.shape))
-        
-        #erosion of binary
-        image_erosion = skm.erosion(binary, skm.square(10))
-        marker , object_count = ndimage.label(image_erosion, np.ones((3,3), bool))
-       
-#===============================================================================
-#         print "there is", object_count, "objects"
-#         
-#         labeled_mask =return split_image np.zeros_like(labeled_image, dtype=np.uint16)
-# 
-#         #
-#         split_image = skm.watershed(-binary, marker, mask=labeled_mask)
-#             
-#         
-#===============================================================================
-        return marker
-    
-    
-    def split_object2(self, labeled_image):
+    def plot_image(self, image_list):
+        for image in image_list:
+            plt.figure()
+            plt.imshow(image)
+            plt.show()
+            
+    def split_object(self, labeled_image):
         """ split object when it's necessary
         """
         
@@ -241,8 +266,11 @@ class MyModule(cpm.CPModule):
         labeled_mask = np.zeros_like(labeled_image, dtype=np.uint16)
         labeled_mask[labeled_image != 0] = 1
         distance = scipym.distance_transform_edt(labeled_image).astype(np.uint16)
-        distance = skr.mean(distance, skm.disk(2))
-         
+        #=======================================================================
+        # binary = np.zeros(np.shape(labeled_image))
+        # binary[labeled_image > 0] = 1
+        #=======================================================================
+        distance = skr.mean(distance, skm.disk(15))
          
         l_max = skr.maximum(distance, skm.disk(5))
         #l_max = skf.peak_local_max(distance, indices=False,labels=labeled_image, footprint=np.ones((3,3)))
@@ -250,15 +278,11 @@ class MyModule(cpm.CPModule):
         
         l_max = skr.maximum(l_max.astype(np.uint8), skm.disk(6))
         
-        import matplotlib.pyplot as plt
+       
         
-        plt.imshow(l_max)
-        plt.figure()
-        marker = ndimage.label(l_max)[0]      
-        print marker
+        marker = ndimage.label(l_max)[0]
         split_image = skm.watershed(-distance, marker)
-        plt.imshow(marker)
-        plt.show()
+        
         return split_image
         
     def smooth_image(self, image, mask, sigma):
@@ -296,3 +320,4 @@ class MyModule(cpm.CPModule):
         #must be change if setting ask for min size of object
         range_min = 40
         return 2.35 * range_min / 3.5
+    
